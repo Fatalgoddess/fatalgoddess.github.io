d11aaa6f7f433163c686e74969a9a294fc61f9eb
Antonia Ivanova <antonia.ivanova09@yahoo.com>
2015-11-25 21:54:23 +0100

    test

---
+++ test.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+	<title> PHP test </title>
+</head>
+<body>
+	<p>This is an HTML line </p>
+	<?php 
+	echo "<p> This is a PHP line</p>";
+	phpinfo();
+	?>
+</body>
+</html>
